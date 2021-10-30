1b08d603a877f965da03f5c1976ba9a0d4455c32
YoNoSoySantiago <santiago.hurtado1@hormail.com>
2021-10-30 16:05:43 -0500

    created scheme of the fnc class

---
+++ backend/FNC.js
@@ -0,0 +1,24 @@
+class FNC{
+    constructor(){
+        this.gramatica = {};
+        this.firstVariable = '';
+    }
+
+    // Funcion que se encarga de agregar una transition a la gramatica
+    // Dando como entrado una variable y una lista de transiciones
+    addTransition(variable, transiciones){
+        //this.gramatica[variable] = transiciones; #TODO: Revisar las condiciones necesarias antes de agregar
+    }
+
+    // Funcion que se encarga de revisar si la gramatica se encuentra en forma normal de Chomsky
+    checkFormaNormalChomsky(){
+
+    }
+
+    // Funcion que se encarga de revisar si una cadena es aceptada por la gramatica correspondiente
+    // Dando como entrada una cadena w
+    // Retorna un booleano indicando si la cadena es aceptada o no
+    checkCadena(w){
+       
+    } 
+}
